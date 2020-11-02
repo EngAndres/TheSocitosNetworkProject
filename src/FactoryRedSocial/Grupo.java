f07d9d59ec8ab78dc0a0d375b3da4a424bc65fa8
casierrav <jerarquia04@gmail.com>
2020-11-02 09:11:12 -0500

    Update TheSocitosProject based on Factory Pattern

---
+++ src/FactoryRedSocial/Grupo.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FactoryRedSocial;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author casierrav
+ */
+public class Grupo implements ComponenteRedSocial{
+   
+    //Atributos del Grupo
+    private String nombre = ""; //este debe ser único e irrepetible
+    private ArrayList<String> usuarios = null;
+    private ArrayList<Estado> estadosEnGrupo = null;
+    
+    
+    /**
+     * Constructor de la Clase
+     * @param nombre
+     */
+    public Grupo(String nombre){
+        this.nombre = nombre;
+        this.usuarios = new ArrayList<>();
+        this.estadosEnGrupo = new ArrayList<>();
+    }
+    
+    
+    /**
+     * 
+     * @param nuevoUsuario
+     * @return 
+     */
+    public String agregarUsuario(String nuevoUsuario){
+        try{
+            this.usuarios.add(nuevoUsuario);
+            return "Proceso Exitoso";
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+            return "Proceso No Exitoso";
+        }
+    }
+    
+    
+    /**
+     * 
+     * @param usuario
+     * @return 
+     */
+    public boolean buscarUsuario(String usuario){
+        try{
+            return this.usuarios.contains(usuario);
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+            return false;
+        }
+    }
+    
+    
+    /**
+     * 
+     * @param usuario
+     * @return 
+     */
+    public String eliminarUsuario(String usuario){
+        try{
+            this.usuarios.remove(usuario);
+            return "Proceso Exitoso";
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+            return "Proceso No Exitoso";
+        }
+    }
+    
+    
+    /**
+     * Método utilizado para agregar un nuevo estado "escrito" por el usuario.
+     * @param nuevoEstado
+     * @return éxito de la operación
+     */
+    public String agregarEstado(Estado nuevoEstado){
+        try{
+            this.estadosEnGrupo.add(nuevoEstado);
+            return "Proceso Exitoso";
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+            return "Proceso No Exitoso.";
+        }
+    }
+    
+    
+    /**
+     * Método que permite obtener los últimos 10 estados que ha publicado el usuario si tiene más de cinco. 
+     * Si tiene menos de cinco estados, retornará los que tenga.
+     * @return arreglo con los últimos estados
+     */
+    public Estado[] ultimosEstados(){
+        int cantidad = (this.estadosEnGrupo.size() < 10) ? this.estadosEnGrupo.size() : 10;
+        Estado[] estados = new Estado[cantidad];
+        
+        for(int i = 0; i < cantidad; i++){
+            estados[i] = this.estadosEnGrupo.get(cantidad - 1 - i);
+        }
+        
+        return estados;
+    }
+    
+    
+    /**
+     * Este método es utilizado para dar formato a la infomración del Grupo y empaquetarlo en un solo String
+     * @return datos del grupo
+     */
+    @Override
+    public String toString(){
+        String grupo = "El grupo tiene por nombre " + this.nombre + ". Se compone por " + this.usuarios.size() +
+                    " usuarios y se han escrito " + this.estadosEnGrupo.size() + " estados.\n";
+        return grupo;
+    }
+}
